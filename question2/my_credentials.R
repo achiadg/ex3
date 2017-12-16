098e6a1e99d93f8d85e14fcf3847276790daab63
achiadg <achiadg@post.bgu.ac.il>
2017-12-16 16:44:24 +0200

    Add files via upload

---
+++ question2/my_credentials.R
@@ -0,0 +1,7 @@
+consumer_key<-"X52uxzboZKvontEkfsLUkcGrV"
+
+consumer_secret<-"ewhRySrNg9iAt64goqU8OCPDOsWTKCqtnl0QGnj5iBlAx9EKcM"
+
+access_token<-"937304105726431232-HnzJ5MhH15cJhjTs15WD7paYOzgC5f4"
+
+access_secret<-"iyPDYJQaGKya4chysYlGC9jInrnp5Wn4A2LGm1T2cnmiK"
